b283a27d98af28b20ed6bddfac042f3707ea9ee5
yusufiwho <muhammedyusuf.h18@gmail.com>
2024-09-14 00:53:47 +0530

    navbar updated

---
+++ script_navbar.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+  
+  // Animate the burger icon
+  menuToggle.classList.toggle('toggle');
+});
